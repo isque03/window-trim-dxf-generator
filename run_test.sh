16905cb020e2b9332e973102a21bbb52d059fd91
isque22 <adam@atomcommerce.io>
2026-04-27 19:24:46 -0400

    Updated to use QCAD on mac

--- run_test.sh
+++ run_test.sh
@@ -8,6 +8,6 @@ source .venv/bin/activate
 python window_trim_dxf_generator.py
 python view_dxf.py window_trim_elevation.dxf
 
-if [[ "$(uname -s)" == "Darwin" ]] && [[ -d "/Applications/LibreCAD.app" ]]; then
-  open -a LibreCAD window_trim_elevation.dxf
+if [[ "$(uname -s)" == "Darwin" ]] && [[ -d "/Applications/QCAD.app" ]]; then
+  open -a /Applications/QCAD.app window_trim_elevation.dxf
 fi
